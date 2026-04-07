cca716bf14dd71a1f895b94fd31cc411f42c3f59
Miguel Garces <sirmardok@gmail.com>
2026-04-07 10:25:24 -0500

    Initial commit

---
+++ src/test/java/com/terpel/estaciones/EstacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terpel.estaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
